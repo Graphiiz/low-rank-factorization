781a892befcf8303d2b1ea339db62a4e059dcfe4
Graphiiz <65876337+Graphiiz@users.noreply.github.com>
2021-04-04 00:59:16 -0400

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -9,32 +9,99 @@ import torchvision
 import torchvision.datasets as datasets
 import torchvision.models as models
 import torchvision.transforms as transforms
+import tensorflow as tf
+from torch.utils.data import Dataset
 from PIL import Image
 import glob
 import os
 
-def train_loader():
-  transform_train = transforms.Compose([
-      transforms.RandomCrop(32, padding=4),
-      transforms.RandomHorizontalFlip(),
-      transforms.ToTensor(),
-      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-  ])
-  trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-  trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=1,pin_memory=True)
+# def train_loader():
+#     transform_train = transforms.Compose([
+#         transforms.RandomCrop(32, padding=4),
+#         transforms.RandomHorizontalFlip(),
+#         transforms.ToTensor(),
+#         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+#     ])
+#     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+#     trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=1,pin_memory=True)
 
-  return trainloader
+#     return trainloader
 
-def test_loader():
-  transform_test = transforms.Compose([
-      transforms.ToTensor(),
-      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-  ])
+# def test_loader():
+#     transform_test = transforms.Compose([
+#         transforms.ToTensor(),
+#         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+#     ])
 
-  testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-  testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=1,pin_memory=True)
+#     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+#     testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=1,pin_memory=True)
 
-  return testloader
+#     return testloader
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+# classes = ('plane', 'car', 'bird', 'cat', 'deer',
+#            'dog', 'frog', 'horse', 'ship', 'truck')
+
+class MNIST(data.Dataset):
+    """MNIST dataset."""
+
+    def __init__(self, X, y, transform=None):
+    
+        self.transform = transform
+        self.X = torch.stack([self.transform(x) for x in X])
+        self.y = y
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return (self.X[idx],self.y[idx])
+
+    
+def create_trainset(dataset_name, batch_size):
+    if dataset_name.lower() == 'cifar10':
+        transform_train = transforms.Compose([transforms.RandomCrop(32, padding=4),transforms.RandomHorizontalFlip(),
+                          transforms.ToTensor(),transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=1,pin_memory=True)
+
+        return trainloader
+
+    if dataset_name.lower() == 'mnist':
+        #TODO fix redundancy of x_test, y_test in case that we need only trainset in create_trainset() and need testset only in create_testset()
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(path="mnist.npz")
+
+        x_train_rescaled = x_train/255
+        x_train_mean = np.mean(x_train_rescaled)
+        x_train_std = np.std(x_train_rescaled)
+
+        y_train = y_train.astype(np.long) #since we create dataset downloaded from tensorflow not pytorch, this cast change type from uint8 to long whuch is compatible with loss function
+
+        transform_train = transforms.Compose([transforms.ToTensor(),transforms.Normalize(x_train_mean, x_train_std),transforms.Pad(padding=2,padding_mode='edge')]) 
+        #must be in order, eg. ToTensor() come first because the following transforms require tensor type data.
+        trainset = MNIST(x_train,y_train,transform_train)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=1,pin_memory=True)
+        return trainloader
+
+def create_testset(dataset_name):
+    if dataset_name.lower() == 'cifar10':
+        transform_test = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+
+        testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=1,pin_memory=True)
+
+        return testloader
+
+    if dataset_name.lower() == 'mnist':
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(path="mnist.npz")
+
+        x_test_rescaled = x_test/255
+        x_test_mean = np.mean(x_test_rescaled)
+        x_test_std = np.std(x_test_rescaled)
+
+        y_test = y_test.astype(np.long)
+        
+        transform_test = transforms.Compose([transforms.ToTensor(),transforms.Normalize(x_test_mean, x_test_std),transforms.Pad(padding=2,padding_mode='edge')])
+        testset = MNIST(x_test,y_test,transform_test)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=1,pin_memory=True)
+        return testloader
--- model.py
+++ model.py
@@ -1,11 +1,14 @@
-'''LeNet in PyTorch.'''
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.init as init
 
+"""
+LeNet with MNIST
+"""
 class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(1, 6, 5) #input is a single channel image -- change according to dataset, now LeNet is specified for mnist dataset only
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1   = nn.Linear(16*5*5, 120)
         self.fc2   = nn.Linear(120, 84)
@@ -21,23 +24,142 @@ class LeNet(nn.Module):
         out = F.relu(self.fc2(out))
         out = self.fc3(out)
         return out
+"""
+VGG with CIFAR-10
+"""
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
 
-class decomp_LeNet(nn.Module):
-    def __init__(self):
-        super(decomp_LeNet, self).__init__()
-        self.conv1 = nn.Sequential(nn.Conv2d(3,1,1,bias=False),nn.Conv2d(1,3,5,bias=False),nn.Conv2d(3,6,1))
-        self.conv2 = nn.Sequential(nn.Conv2d(6,2,1,bias=False),nn.Conv2d(2,5,5,bias=False),nn.Conv2d(5,16,1))
-        self.fc1   = nn.Sequential(nn.Linear(400,24,bias=False),nn.Linear(24,120))
-        self.fc2   = nn.Sequential(nn.Linear(120,4,bias=False),nn.Linear(4,84))
-        self.fc3   = nn.Sequential(nn.Linear(84,7,bias=False),nn.Linear(7,10))
+class VGG(nn.Module):
+    def __init__(self, name='VGG16'):
+          super(VGG, self).__init__()
+          self.name = name
+          self.features = self.make_layers()
+          self.fc1 = nn.Linear(512,10)
 
     def forward(self, x):
-        out = F.relu(self.conv1(x))
-        out = F.max_pool2d(out, 2)
-        out = F.relu(self.conv2(out))
-        out = F.max_pool2d(out, 2)
+        out = self.features(x)
         out = out.view(out.size(0), -1)
-        out = F.relu(self.fc1(out))
-        out = F.relu(self.fc2(out))
-        out = self.fc3(out)
+        out = self.fc1(out)
+        out = F.softmax(out,dim=1)
         return out
+
+    def make_layers(self):
+        in_channels = 3
+        layers = []
+        config = cfg[self.name]
+        for c in config:
+            if c == 'M':
+                layers += [nn.MaxPool2d(2,2)]
+            else:
+                conv_layer = nn.Conv2d(in_channels=in_channels,out_channels=c,kernel_size=3,stride=1,padding=1,dilation=1)
+                batch_norm = nn.BatchNorm2d(num_features=c)
+                activation = nn.ReLU(inplace=True) 
+                #inplace=True means that it will modify the input directly, without allocating any additional output. True -- save memory
+                layers += [conv_layer,batch_norm,activation]
+                in_channels = c #update -- current layer output channels = next layer input channel
+
+        return nn.Sequential(*layers)
+"""
+ResNet20 with CIFAR-10
+"""
+__all__ = ['ResNet', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1202']
+
+def _weights_init(m):
+    classname = m.__class__.__name__
+    #print(classname)
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+        init.kaiming_normal_(m.weight)
+
+class LambdaLayer(nn.Module):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
+
+    def forward(self, x):
+        return self.lambd(x)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, option='A'):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            if option == 'A':
+                """
+                For CIFAR10 ResNet paper uses option A.
+                """
+                self.shortcut = LambdaLayer(lambda x:
+                                            F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
+            elif option == 'B':
+                self.shortcut = nn.Sequential(
+                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                     nn.BatchNorm2d(self.expansion * planes)
+                )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.linear = nn.Linear(64, num_classes)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+"""
+Create model
+"""
+def create_model(model_name):
+    if model_name.lower() == 'lenet':
+        return LeNet()
+
+    elif model_name.lower() == 'vgg16':
+        return VGG('VGG16')
+    
+    elif model_name.lower() == 'resnet20':
+        return ResNet(BasicBlock, [3, 3, 3])
+    
+    else:
+        exit(0)
